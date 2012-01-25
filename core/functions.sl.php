1cc072ef94a91da8588acd1820f00992abec5196
cybersprocket <cybersprocket@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-25 17:02:51 +0000

    v2.3.1
    git-svn-id: https://plugins.svn.wordpress.org/store-locator-le/tags/2.3.1@495269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core/functions.sl.php
+++ core/functions.sl.php
@@ -190,7 +190,7 @@ function initialize_variables() {
     
     $radii=get_option('sl_map_radii');
     if (empty($radii)) {
-        add_option('sl_map_radii', "1,5,10,25,(50),100,200,500");
+        add_option('sl_map_radii', "10,25,50,100,(200),500");
         $radii=get_option('sl_map_radii');
         }
 }
@@ -750,7 +750,7 @@ function add_admin_stylesheet() {
 function set_query_defaults() {
 	global $where, $o, $d;
 	
-	$qry = isset($_GET['q']) ? $_GET['q'] : '';
+	$qry = isset($_REQUEST['q']) ? $_REQUEST['q'] : '';
 	$where=($qry!='')? 
 	        " WHERE ".
 	        "sl_store    LIKE '%$qry%' OR ".
