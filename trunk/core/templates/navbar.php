e20af20c0f21fa9ca9812958608852e2fdf57159
cybersprocket <cybersprocket@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-13 19:11:05 +0000

    v3.0.7
    git-svn-id: https://plugins.svn.wordpress.org/store-locator-le/tags/3.0.7@557507 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/core/templates/navbar.php
@@ -0,0 +1,26 @@
+<?php
+/****************************************************************************
+ ** file: core/templates/navbar.php
+ **
+ ** The top Store Locator Settings navigation bar.
+ ***************************************************************************/
+ 
+ global $slplus_plugin;
+?>
+<script src="<?php echo SLPLUS_COREURL;?>js/functions.js"></script>
+<ul>
+    <a href="<?php echo SLPLUS_ADMINPAGE;?>view-locations.php"><li class='like-a-button'>Locations: Manage</li></a>
+    <a href="<?php echo SLPLUS_ADMINPAGE;?>add-locations.php"><li class='like-a-button'>Locations: Add</li></a>
+    <a href="<?php echo SLPLUS_ADMINPAGE;?>map-designer.php"><li class='like-a-button'>Settings: Map</li></a>
+    <a href="<?php echo admin_url(); ?>options-general.php?page=csl-slplus-options"><li class='like-a-button'>Settings: General</li></a>    
+    <?php 
+    //--------------------------------
+    // Pro Version : Show Reports Tab
+    //
+    if ($slplus_plugin->license->AmIEnabled(true, "SLPLUS")) {      
+        print '<a href="'.SLPLUS_PLUSPAGE.'reporting.php"><li class="like-a-button">Reports</li></a>';
+    }
+    ?>    
+</ul>
+
+
